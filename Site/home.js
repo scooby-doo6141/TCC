70cfac27f5f41a42d2342dc207598a51040abb70
scooby-doo6141 <kaiue6141@gmail.com>
2025-08-31 15:10:31 -0300

    Commit inicial

---
+++ Site/home.js
@@ -0,0 +1,10 @@
+
+
+function cadAparecer(){
+    var cad = document.getElementById("cad");
+    cad.style.display="block";
+}
+function cadDesaparecer(){
+     var cad = document.getElementById("cad");
+     cad.style.display="none";
+}
